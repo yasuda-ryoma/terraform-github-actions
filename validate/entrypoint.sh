fdb08a577d4046a451cb837a81c445dccf0ecdfc
hayashi taiki <hayashitaiki@team-lab.com>
2019-01-27 03:47:24 +0900

    tf dir auto search

--- validate/entrypoint.sh
+++ validate/entrypoint.sh
@@ -1,30 +1,44 @@
 #!/bin/sh
 set -e
-cd "${TF_ACTION_WORKING_DIR:-.}"
-
-WORKSPACE=${TF_ACTION_WORKSPACE:-default}
-terraform workspace select "$WORKSPACE"
-
-set +e
-OUTPUT=$(sh -c "terraform validate -no-color $*" 2>&1)
-SUCCESS=$?
-echo "$OUTPUT"
-set -e
-
-if [ $SUCCESS -eq 0 ]; then
-    exit 0
+if $AUTO_SEARCH_TF_DIR ; then
+    WORKING_DIRS=$(sh -c 'find . -type f -name "*.tf" -exec dirname {} \;|sort -u'  2>&1)
+else
+    WORKING_DIRS="${TF_ACTION_WORKING_DIR:-.}"
 fi
 
-if [ "$TF_ACTION_COMMENT" = "1" ] || [ "$TF_ACTION_COMMENT" = "false" ]; then
+WORKSPACE=${TF_ACTION_WORKSPACE:-default}
+
+# Iterate through each directory and build up a comment.
+SUCCESS=0
+for dir in $WORKING_DIRS; do
+VARIDATE_COMMENT=$(sh -c "cd $dir && terraform workspace select $WORKSPACE && terraform validate -no-color $*" 2>&1)
+RETURN=$?
+echo "$dir"
+echo "$VARIDATE_COMMENT"
+if [ $RETURN -eq 0 ]; then
+    continue
+fi
+SUCCESS=$RETURN
+VARIDATE_OUTPUT="$VARIDATE_OUTPUT
+<summary><code>$dir</code></summary>
+
+\`\`\`
+$VARIDATE_COMMENT
+\`\`\`
+
+"
+done
+
+if [ $SUCCESS -eq 0 ]; then
     exit $SUCCESS
 fi
 
 COMMENT="#### \`terraform validate\` Failed
-\`\`\`
-$OUTPUT
-\`\`\`"
+$LINT_OUTPUT
+"
 PAYLOAD=$(echo '{}' | jq --arg body "$COMMENT" '.body = $body')
 COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .pull_request.comments_url)
 curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL" > /dev/null
 
 exit $SUCCESS
+
